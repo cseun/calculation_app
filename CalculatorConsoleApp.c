f53ed59ba4084c841843a8f2de2dde401f26fd33
sein <cseeun98@gmail.com>
2025-10-15 11:10:49 +0900

    Fix calculation proceeding without operators

--- CalculatorConsoleApp.c
+++ CalculatorConsoleApp.c
@@ -16,7 +16,6 @@ int main()
 		printf("- 허용된 연산문자 외의 문자는 제외하고 계산됩니다. \n");
 		printf("- 첫 글자가 종료문자(X, X)인 경우 프로그램이 종료됩니다. \n:");
 
-		int input = 0;
 		boolean isFirst = TRUE;
 		char* validOperators = "*/+-";
 		char* exits = "xX";
@@ -30,10 +29,11 @@ int main()
 		}
 
 		// [4-2] === 사용자 입력 및 저장 ===
-		while ((input = getchar()) != '\n' && input != EOF) {
+		int ch = 0;
+		while ((ch = getchar()) != '\n' && ch != EOF) {
 			// 첫 문자 종료키 처리
 			if (isFirst) {
-				if (strchr(exits, input)) {
+				if (strchr(exits, ch)) {
 					printf("프로그램 종료\n");
 					Sleep(2000);
 					return 1;
@@ -41,7 +41,7 @@ int main()
 				isFirst = FALSE;
 			}
 
-			if (isdigit(input) || strchr(validOperators, input)) {
+			if (isdigit(ch) || strchr(validOperators, ch)) {
 				if (exprLen + 1 >= exprSize) {
 					exprSize *= 2;
 					char* temp = (char*)realloc(expression, sizeof(char) * exprSize);
@@ -52,18 +52,18 @@ int main()
 					}
 					expression = temp;
 				}
-				expression[exprLen++] = (char)input;
+				expression[exprLen++] = (char)ch;
 			}
 		}
+		expression[exprLen] = '\0';
 
-		if (input == EOF && ferror(stdin)) {
+		if (ch == EOF && ferror(stdin)) {
 			printf("입력 값 읽어오는 중 오류 발생. 재입력 해주세요.");
 			clearerr(stdin);
 			free(expression);
 			continue;
 		}
 
-		expression[exprLen] = '\0';
 		if (exprLen == 0) {
 			printf("저장된 계산식이 없습니다. 다시 입력해주세요.\n\n");
 			continue;
@@ -110,26 +110,40 @@ int main()
 		int numberIdx = 0;
 		int operatorIdx = 0;
 		char* expressionPtr = expression;
-		while (*expressionPtr) {
+		while (*expressionPtr) { // 표현식이 존재할때 까지
+			char* endptr;
+			int num = strtol(expressionPtr, &endptr, 10);
+			// 포인터가 가리키는 값이 숫자인 경우
 			if (isdigit((unsigned char)*expressionPtr)) {
-				char* endptr;
-				int num = strtol(expressionPtr, &endptr, 10);
-				numbers[numberIdx++] = num;
+				numbers[numberIdx] = num;
 				expressionPtr = endptr;
-
-				operators[operatorIdx++] = *expressionPtr;
-				expressionPtr++;
+				// 숫자 다음 포인터 값이 연산자인 경우
+				if (*expressionPtr && strchr(validOperators, *expressionPtr)) {
+					operators[operatorIdx] = *expressionPtr;
+					expressionPtr++;
+					operatorIdx++;
+				}
+				numberIdx++;
+			}
+			else {
+				break;
 			}
 		}
 
-		if (numberIdx < 2 || operatorIdx < 1) {
-			printf("계산에 필요한 숫자 혹은 연산자가 부족.\n\n");
+		int numberCount = numberIdx;
+		int operatorCount = operatorIdx;
+
+		if (numberCount < 2 || operatorCount < 1) {
+			printf("계산에 필요한 숫자 혹은 연산자가 부족. 재입력 해주세요.\n\n");
 			continue;
 		}
 
+		// [7번] === 계산식 출력 및 계산 ===
+		printf("필터링된 계산식: %s\n", expression);
+
 		// 계산 처리
 		int i = 0;
-		while (i < operatorIdx) {
+		while (i < operatorCount) {
 			if (operators[i] == '*' || operators[i] == '/') {
 				int a = numbers[i];
 				int b = numbers[i + 1];
@@ -138,28 +152,41 @@ int main()
 					printf("0으로 나눌 수 없습니다\n\n");
 					goto HERE; //입력으로 돌아감
 				}
+
+				// i 와 i+1 의 계산 결과값을 i에 저장
 				numbers[i] = (operators[i] == '*') ? a * b : a / b;
-				for (int j = i + 1; j < numberIdx - 1; j++)
+
+				// i+1 부터 좌 shift 
+				for (int j = i + 1; j < numberCount - 1; j++)
 				{
 					numbers[j] = numbers[j + 1];
 				}
-				for (int j = i; j < operatorIdx - 1; j++)
+
+				// i 부터 좌 shift
+				for (int j = i; j < operatorCount - 1; j++)
 				{
 					operators[j] = operators[j + 1];
 				}
-				numberIdx--;
-				operatorIdx--;
+
+				// 줄인 범위에서 반복되도록 변경
+				numberCount--;
+				operatorCount--;
 			}
 			else {
+				// 곱셈, 나눗셈이 아닌 경우 다음 연산자 조회
 				i++;
 			}
 		}
 
-		printf("필터링된 계산 수식: %s\n", expression);
+		// 덧셈, 뺄셈 계산
 		int result = numbers[0];
-		for (int j = 0; j < operatorIdx; j++) {
-			if (operators[j] == '+') result += numbers[j + 1];
-			else result -= numbers[j + 1];
+		for (int j = 0; j < operatorCount; j++) {
+			if (operators[i] == '+') {
+				result += numbers[i + 1];
+			}
+			else {
+				result -= numbers[i + 1];
+			}
 		}
 
 		printf("계산 결과: %d \n\n", result);
